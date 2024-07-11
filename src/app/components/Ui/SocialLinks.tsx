2dea26cd6fef7f9e232ef130ce031347175195ff
abhi-saini1 <abhishek977933@gmail.com>
2024-07-11 09:54:40 +0530

    commit

--- src/app/components/Ui/SocialLinks.tsx
+++ src/app/components/Ui/SocialLinks.tsx
@@ -6,12 +6,11 @@ const SocialLinks = () => {
     <div className='fixed top-4 left-5'> 
         <div className='flex items-center justify-center gap-2'>
         <Link href='/' className='w-[40px] h-[40px]' target='_blank' rel='noopener noreferrer'  >
-            <Image src={'/instagram.png'} alt='Instagram' width={40} height={40}/>
+            <Image src={'/Instagram.png'} alt='Instagram' width={40} height={40}/>
         </Link>
         <Link href='https://www.linkedin.com/in/abhishek-saini-292039310/'
          className='w-[40px] h-[40px]' target='_blank' rel='noopener noreferrer'>
-            
-            <Image src={'/linkedin.png'} alt='LinkedIn' width={40} height={40}/>
+            <Image src={'/Linkedin.png'} alt='LinkedIn' width={40} height={40}/>
         </Link>
         </div>
     </div>
